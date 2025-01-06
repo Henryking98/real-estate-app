d265dc18e5638c0e8e1ad25283b070567970ee9b
Henry <94872110+Henryking98@users.noreply.github.com>
2025-01-06 03:04:29 -1100

    resolved nativewind error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,10 +2,8 @@ import {DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
-import 'react-native-reanimated';
-import "./globals.css";
+import "./global.css";
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
